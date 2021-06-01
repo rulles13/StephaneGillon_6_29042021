91386d192993b7a8c01b6cde5d2410df26710209
Stephane Gillon <stephanegillon@hotmail.con>
2021-06-01 11:12:17 +0200

    corrections sécurité

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,12 @@ require('dotenv').config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_PASS);
-    console.log(process.env.SECRET_PASS);
-    
+    const decodedToken = jwt.verify(token, process.env.SECRET_PASS);    
     const userId = decodedToken.userId;
+    
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
-      console.log(req.body.userId);
       next();
     }
   } catch {
--- models/User.js
+++ models/User.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+var pwdValidate = require('mongoose-validator');
+
+var pwdValidator = [
+  pwdValidate({
+    validator: 'isLength',
+    arguments: [12, 50], // lenght must be between 12 and 50 characters
+    message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
+  }),
+  pwdValidate({
+    validator: 'matches',
+    arguments: /^[a-z\d\-_\s]+$/i, // Regex for characters you can use
+    message: 'Name should contain alpha-numeric characters only',
+  }),
+]
+
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true, pwdValidate: pwdValidator }
 });
 
 userSchema.plugin(uniqueValidator);
